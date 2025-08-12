5aa29db1dd2e73965430641d6e6939f732490715
anthon-louise <anthonlouisenoynay123@gmail.com>
2025-08-12 10:36:46 +0800

    feature: db setup

---
+++ backend/config/db.js
@@ -0,0 +1,47 @@
+const mysql = require('mysql2/promise')
+require('dotenv').config()
+
+const pool = mysql.createPool({
+    user: process.env.USER,
+    name: process.env.NAME,
+    host: process.env.HOST,
+    password: process.env.PASSWORD,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+})
+
+async function initializeDB() {
+    try {
+        const connection = await pool.getConnection()
+
+        await connection.query(`
+            CREATE TABLE IF NOT EXISTS users (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                username VARCHAR(255) UNIQUE,
+                password VARCHAR(255)
+            )
+            `)
+
+        await connection.query(`
+            CREATE TABLE IF NOT EXISTS notes (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                title VARCHAR(255),
+                content TEXT,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                user_id INT,
+                FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+            )
+            `)
+
+
+        console.log('Database connected')
+        connection.release()
+    } catch (err) {
+        throw new Error(`Initializing database failed: ${err.message}`)
+    }
+}
+
+initializeDB()
+
+module.exports = pool
