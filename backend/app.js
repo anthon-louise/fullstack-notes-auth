0d611638e12a30d2e47b0abcd13e8a244fa152f8
anthon-louise <anthonlouisenoynay123@gmail.com>
2025-08-14 04:40:03 +0800

    feature: register user frontend functionalities

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const userRoutes = require('./routes/user.routes')
 const errorHandler = require('./middlewares/errorHandler')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/users', userRoutes)
 
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,22 +1,35 @@
 import { useState } from "react";
 import Input from "../components/Input";
 import Button from "../components/Button";
+import { registerUser } from "../services/userService";
 
 function RegisterPage() {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
 
-    const handleSave = () => {
+    const handleSave = async () => {
+        const data = await registerUser({ username, password })
+        if (data.success) {
+            alert(data.message)
+            setUsername('')
+            setPassword('')
+            console.log('success')
+        } else {
+            alert(data.message)
+            console.log('unsuccess')
+        }
+
 
     }
 
     return (
         <div>
+            <h2>Register</h2>
             <Input label='Username' value={username} onChange={setUsername} />
             <br />
             <Input label='Password' value={password} onChange={setPassword} />
             <br />
-            <Button label='Submit' type='submit' onClick={handleSave}/>
+            <Button label='Submit' type='submit' onClick={handleSave} />
         </div>
     )
 }
