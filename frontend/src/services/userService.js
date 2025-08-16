641def904a8b9ea0fba9b344a23333e21d7d03be
anthon-louise <anthonlouisenoynay123@gmail.com>
2025-08-16 18:25:46 +0800

    feature: login service

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -3,12 +3,24 @@ const API_URL = 'http://localhost:5000/users'
 
 export async function registerUser (credentials) {
     try {
-        const res = await axios.post(`${API_URL}/register`, credentials)
+        const res = await axios.post(`${API_URL}/register`, credentials, {withCredentials: true})
         return res.data
     } catch (err) {
         if (err.response && err.response.data) {
             return err.response.data
         }
         return {success: false, message: err.message}
+    }
+}
+
+export async function loginUser (credentials) {
+    try {
+        const res = await axios.post(`${API_URL}/login`, credentials, {withCredentials: true})
+        return res.data
+    } catch (err) {
+        if (err.response && err.response.data) {
+            return err.response.data
+        }
+        return {sucess: false, message: err.message}
     }
 }
