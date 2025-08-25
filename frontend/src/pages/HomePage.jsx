20020801fd3197e98a26e4f2870615ec0e1f8732
anthon-louise <anthonlouisenoynay123@gmail.com>
2025-08-26 05:49:58 +0800

    feature: create note ui

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react"
+import Input from "../components/Input"
+import Button from "../components/Button"
+import { createNote } from "../services/noteService"
 
 function HomePage() {
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+
+    const handleSaveNote = async () => {
+        console.log(title, content)
+    }
 
     return (
         <div>
-            <p>Hello world</p>
+            <Input value={title} label='Title' onChange={setTitle}/>
+            <Input value={content} label='Content' onChange={setContent}/>
+            <Button label='Add Note' type='submit' onClick={handleSaveNote}/>
         </div>
     )
 }
